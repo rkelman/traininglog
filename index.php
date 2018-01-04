47adc8ecb147d7383204b38d869a167180e96bd9
Robb Kelman <robb.kelman@gmail.com>
2018-01-04 04:50:00 +0000

    initial index file

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+echo "<html>\n";
+echo "<body>\n";
+echo "<form>\n";
+echo "Distance (mi/yds): ";
+echo "<input type=\"text\" name=\"Distance\"><BR>\n";
+echo "Elapsed Time (min): ";
+echo "<input type=\"text\" name=\"Distance\"><BR>\n";
+echo "Sport: ";
+echo "<select name=\"sport\">\n";
+echo "  <option value=\"Cycling\">Cycling</option>\n";
+echo "  <option value=\"Run\">Running</option>\n";
+echo "  <option value=\"Swim\">Swim</option>\n";
+echo "  <option value=\"Circuit\">Circuit</option>\n";
+echo "</select>\n";
+echo "</form>\n";
+echo "</body>\n";
+echo "</html>"
+?>
