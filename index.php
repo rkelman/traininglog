a0ff681e898b82beb6fc5498cbb4e510c6bab501
Robb Kelman <robb.kelman@gmail.com>
2018-01-05 01:28:57 +0000

    troubleshooting totals 15

--- index.php
+++ index.php
@@ -53,9 +53,7 @@ echo "<input type=\"submit\" value=\"Log Training\">";
 echo "</form>\n";
 echo "<BR><BR>";
 echo "Annual Totals:<BR>\n";
-$total_sql = "SELECT type, sum(distance) sum_dist
-        FROM training_log
-        GROUP BY type";
+$total_sql = "SELECT type, sum(distance) sum_dis FROM training_log GROUP BY type";
 
 echo $total_sql;
 $tot_result = $conn->query($total_sql);
@@ -67,8 +65,8 @@ if (!$result = $conn->query($total_sql)) {
   echo $total_sql;
 }
 */
-echo "Test: ".$tot_result->num_rows;
-
+//echo "Test: ".$tot_result->num_rows;
+/*
 if ($tot_result->num_rows > 0) {
   // output data of each row
   while($row = $tot_result->fetch_assoc()) {
@@ -78,13 +76,13 @@ if ($tot_result->num_rows > 0) {
     } else {
       $dist_unit = 'Yds';
     }
-    */
+
     echo $row['type'].": ".$row['sum_dist']."<BR>\n";
   }
 } else {
   echo "Sorry - no training logged this year<BR>\n";
 }
-
+*/
 $conn->close();
 
 echo "</body>\n";
