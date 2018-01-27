5b98cd44ffdf0a6c944b4e06956c29766d14b7b1
Robb Kelman <robb.kelman@gmail.com>
2018-01-27 18:41:59 +0000

    mv reseta-php to reset-php and updated all references to reseta

--- reset.php
+++ reset.php
@@ -4,72 +4,22 @@ include 'userkey.php';
 
 $conn = connectDB();
 
-//if password is being sent and we are on the last step
-if (isset($_POST["password"])) {
-  $username = $_POST["email"];
-  $key = $_POST["key"];
-  $passwd = $_POST["password"];
-  $passwd2 = $_POST["password_conf"];
-// check values and insert into db
+//print_r($_GET);
 
-//if the user has received the key in email
-} elseif (isset($_GET["key"])) {
-  $username = $_GET["name"];
-  $key = $_GET["key"];
-
-  if (validateUserKey($username, $key)) {
-    echo "<html>\n";
-    echo "<head>\n<link rel=\"stylesheet\" href=\"traininglog.css\">\n";
-    echo "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n";
-    echo "</head>\n";
-    echo "<body>\n";
-    echo "<form action=\"reset.php\" method=\"post\">\n";
-    echo "New Password: ";
-    echo "<input type=\"passord\" name=\"password\"><BR>\n";
-    echo "Confirm Password: ";
-    echo "<input type=\"passord\" name=\"password\"><BR>\n";
-    echo "<input type=\"submit\" name=\"Reset Password\"><BR>\n";
-    echo "</form>\n";
-    echo "</body>\n";
-    echo "</html>";
-  } else {
-    //else if key is invalid - give user error and link to start over
-    echo "<html>\n";
-    echo "<head>\n<link rel=\"stylesheet\" href=\"traininglog.css\">\n";
-    echo "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">";
-    echo "</head>\n";
-    echo "<body>\n";
-    echo "<neg_mesg>We're sorry your key is has either expired or is invalid</neg_mesage>\n";
-    echo "<a href=\"reset.php\">Please request a new key to reset your password</href>\n";
-    echo "</body>\n";
-    echo "</html>";
+//if no post or get; first time to page
+if (!isset($_POST["email"]) && !isset($_GET['mail'])) {
+  echo "<html>\n";
+  echo "<head>\n<link rel=\"stylesheet\" href=\"traininglog.css\">\n";
+  echo "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">";
+  echo "</head>\n";
+  echo "<body>\n";
+  if (isset($_GET["err"])) {
+    if ($_GET["err"]=="InvalidKey") {
+      echo "<neg_mesg>Sorry, The key that you used appears to be expired, please request a new one.</neg_mesg><BR>";
+    } elseif ($_GET["err"]=="InvalidName") {
+      echo "<neg_mesg>Sorry, The email you entered is not registered to a Traininglog user.</neg_mesg><BR>";
+    }
   }
-
-//if the user has simply requested the reset
-} elsif (isset($_POST["email"])) {
-  $username = $_POST["email"];
-
-  $key = createUserKey($username);
-  emailUserKey($key, $username);
-
-  echo "<html>\n";
-  echo "<head>\n<link rel=\"stylesheet\" href=\"traininglog.css\">\n";
-  echo "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">";
-  echo "</head>\n";
-  echo "<body>\n";
-  echo "A link to reset your password has been sent to your email<BR><BR>";
-  echo "Please check your email to complete the reset process<BR>\n";
-  echo "</body>\n";
-  echo "</html>";
-
-//if we're on the first step - no email has been requested
-} else {
-
-  echo "<html>\n";
-  echo "<head>\n<link rel=\"stylesheet\" href=\"traininglog.css\">\n";
-  echo "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">";
-  echo "</head>\n";
-  echo "<body>\n";
   echo "Please enter e-mail to reset password<BR><BR>";
   echo "<form action=\"reset.php\" method=\"post\">\n";
   echo "eMail: ";
@@ -78,7 +28,63 @@ if (isset($_POST["password"])) {
   echo "</form>\n";
   echo "</body>\n";
   echo "</html>";
+}  elseif (isset($_POST["email"])) {
+  $username = $_POST["email"];
+  //echo $username."<BR>\n";
+  $res_sql = "SELECT * from tlUsers where email = '".$username."'";
+  //echo $res_sql;
 
+  if (!$res_result = $conn->query($res_sql)) {
+    // Oh no! The query failed.
+    echo "<neg_mesg>Sorry, Traininglog is experiencing problems.</neg_mesg><BR>";
+    echo $res_sql;
+  }
+
+  if ($res_result->num_rows > 0) {
+    $key = createUserKey($username);
+    //echo $key;
+    mailUserKey($username, $key);
+
+    echo "<html>\n";
+    echo "<head>\n<link rel=\"stylesheet\" href=\"traininglog.css\">\n";
+    echo "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">";
+    echo "</head>\n";
+    echo "<body>\n";
+    echo "A link to reset your password has been sent to your email<BR><BR>";
+    echo "Please check your email to complete the reset process<BR>\n";
+    echo "</body>\n";
+    echo "</html>";
+  } else {
+    header:('Location: reset.php?err=InvalidName');
+  }
+} elseif (isset($_GET['mail']) && isset($_GET['key'])) {
+  $mailID = $_GET['mail'];
+  $keyID = $_GET['key'];
+  if (validateUserKey($mailID, $keyID)) {
+    //Allow user to enter new password_conf
+    echo "<html>\n";
+    echo "<head>\n<link rel=\"stylesheet\" href=\"traininglog.css\">\n";
+    echo "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">";
+    echo "</head>\n";
+    echo "<body>\n";
+    echo "<form action=\"resetpass.php\" method=\"post\">\n";
+    echo "Password: ";
+    echo "<input type=\"password\" name=\"password\"><BR>\n";
+    echo "Confirm Password: ";
+    echo "<input type=\"password\" name=\"password_conf\"><BR>\n";
+    echo "<input type=\"hidden\" id=\"mailID\" name=\"mail\" value=\"".$mailID."\">\n";
+    echo "<input type=\"hidden\" id=\"keyID\" name=\"key\" value=\"".$keyID."\">\n";
+    echo "<input type=\"submit\" name=\"Reset Password\"><BR>\n";
+    echo "</form>\n";
+    echo "</body>\n";
+    echo "</html>";
+  } else {
+    //otherwise let the user know their key is invalid
+    //echo "email: ".$mailID."<BR>\n";
+    //echo "emailed key: ".$keyID."<BR>\n";
+    //echo "function: ".hash('gost',$mailID.date('z'));
+    header('Location: reset.php?err=InvalidKey');
+  }
 }
 
 $conn->close();
