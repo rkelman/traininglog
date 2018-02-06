413446283ad8821180ccd3b3c667a650649d959c
Robb Kelman <robb.kelman@gmail.com>
2018-02-06 04:51:21 +0000

    empty worked - commented out troubleshooting messages

--- index.php
+++ index.php
@@ -38,7 +38,7 @@ echo "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">
 echo "</head>\n";
 echo "<body>\n";
 //troubleshooting insert statements
-echo $ins_sql."; distance='".$_POST["distance"]."'";
+//echo $ins_sql."; distance='".$_POST["distance"]."'";
 if ($ins_trainlog) {
   if ($sport == 'Cycling') {
     $activity = 'ride';
