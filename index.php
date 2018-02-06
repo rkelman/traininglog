7ac520f14a782a729f9ef191b9f1b2473b5dd637
Robb Kelman <robb.kelman@gmail.com>
2018-02-06 04:41:44 +0000

    changed distance to quoted 0 from 0 without quotes on null

--- index.php
+++ index.php
@@ -38,7 +38,7 @@ echo "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">
 echo "</head>\n";
 echo "<body>\n";
 //troubleshooting insert statements
-//echo $ins_sql;
+echo $ins_sql;
 if ($ins_trainlog) {
   if ($sport == 'Cycling') {
     $activity = 'ride';
